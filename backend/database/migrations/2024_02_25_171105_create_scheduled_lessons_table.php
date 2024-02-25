f911558d313f20d57155ddedb301acd461f93bd8
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-02-25 14:23:41 -0300

    schedule lessons migration

---
+++ backend/database/migrations/2024_02_25_171105_create_scheduled_lessons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScheduledLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scheduled_lessons', function (Blueprint $table) {
+            $table->id();
+            $table->integer('course_lesson_id')->unsigned();
+            $table->foreign('course_lesson_id')->references('id')->on('course_lessons');
+            $table->integer('klass_id')->unsigned();
+            $table->foreign('klass_id')->references('id')->on('klass');
+            $table->datetime('datetime');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scheduled_lessons');
+    }
+}
