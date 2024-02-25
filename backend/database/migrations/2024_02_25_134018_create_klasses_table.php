6b2dc3fe270bc1c2d0806adc52409c27271aae21
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-02-25 10:41:44 -0300

    initial migrations

---
+++ backend/database/migrations/2024_02_25_134018_create_klasses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKlassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('klasses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('courses_id')->unsigned();
+            $table->foreign('courses_id')->references('id')->on('courses');
+            $table->integer('teacher_id')->unsigned();
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('klasses');
+    }
+}
