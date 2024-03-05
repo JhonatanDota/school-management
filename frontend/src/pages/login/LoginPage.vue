1af5fee57c0798c0a14bb116162fb538626eaf57
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-03-05 19:31:49 -0300

    login page init

--- frontend/src/pages/login/LoginPage.vue
+++ frontend/src/pages/login/LoginPage.vue
@@ -1,5 +1,40 @@
 <template>
-  <h1 class="text-red-500">Login</h1>
+  <div class="mx-auto mt-16 flex flex-col gap-6 p-6 bg-[#222D32] rounded-md">
+    <div class="flex justify-between items-center gap-3 p-3 bg-[#7745a5]">
+      <UserRoundedIcon fill="white" class="w-6 h-5" />
+      <input
+        class="text-base text-white font-normal focus:outline-none rounded-sm bg-transparent placeholder-white/70"
+        type="text"
+        placeholder="e-mail"
+      />
+    </div>
+
+    <div class="flex justify-between items-center gap-3 p-3 bg-[#7745a5]">
+      <LockIcon fill="white" class="w-6 h-6" />
+      <input
+        class="text-base text-white font-normal focus:outline-none rounded-sm bg-transparent placeholder-white/70"
+        type="text"
+        placeholder="password"
+      />
+    </div>
+
+    <div class="flex items-center justify-between text-sm">
+      <div class="flex gap-2">
+        <input id="checkbox-remember" class="" type="checkbox" />
+        <label for="checkbox-remember" class="text-gray-300"
+          >Lembrar usuário ?</label
+        >
+      </div>
+      <button class="font-semibold text-[#3df85cb2]">Registrar</button>
+    </div>
+
+    <button class="w-full text-sm p-2 uppercase font-bold bg-gray-200">
+      Login
+    </button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UserRoundedIcon from "@/icons/UserRoundedIcon.vue";
+import LockIcon from "@/icons/LockIcon.vue";
+</script>
