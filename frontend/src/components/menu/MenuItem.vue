5457d2a58a2d8e3a02b29a224f2260bff5fddfc7
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-03-17 16:08:57 -0300

    frontend: change menu design

--- frontend/src/components/menu/MenuItem.vue
+++ frontend/src/components/menu/MenuItem.vue
@@ -1,13 +1,9 @@
 <template>
   <div
-    class="w-full flex items-center gap-2 md:gap-4 p-3 md:p-5 hover:bg-[#632C96] transition-colors duration-100 cursor-pointer"
-    :class="[isMenuOpen ? 'justify-start' : 'justify-center']"
+    class="w-full flex justify-center items-center gap-2 md:gap-4 p-3 md:p-5 hover:bg-[#632C96] transition-colors duration-100 cursor-pointer"
   >
-    <icon fill="white" color="red" />
-    <h3
-      v-if="isMenuOpen"
-      class="text-base md:text-lg font-bold text-white"
-    >
+    <icon v-if="!isMenuOpen" fill="white" color="red" />
+    <h3 v-if="isMenuOpen" class="text-base md:text-lg font-bold text-white">
       {{ name }}
     </h3>
   </div>
