4017db46f003bacaf0193e335d26ba2586fb4367
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-07-11 19:40:43 -0300

    frontend teacher: remove id from table

--- frontend/src/columns/ColumnsTeacher.ts
+++ frontend/src/columns/ColumnsTeacher.ts
@@ -1,9 +1,8 @@
 import { ThModel } from "@/models/DataTableModel";
 
-export const teacherTdKeys: string[] = ["id", "name", "email", "createdAt"];
+export const teacherTdKeys: string[] = ["name", "email", "createdAt"];
 
 export const teacherThList: ThModel[] = [
-  { text: "Identificador" },
   { text: "Nome" },
   { text: "Email" },
   { text: "Criado em" },
