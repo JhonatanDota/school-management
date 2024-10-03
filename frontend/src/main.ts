0353e4d535711372bb8feea0388d19b9186e8d4a
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-10-03 20:29:06 -0300

    frontend lib: add slick sort

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,12 +3,14 @@ import App from "./App.vue";
 import { MotionPlugin } from "@vueuse/motion";
 import router from "./router";
 import Vue3Toasity, { type ToastContainerOptions } from "vue3-toastify";
+import { plugin as Slicksort } from "vue-slicksort";
 import { createPinia } from "pinia";
 import "./index.css";
 
 createApp(App)
   .use(router)
   .use(MotionPlugin)
+  .use(Slicksort)
   .use(createPinia())
   .use(Vue3Toasity, {
     autoClose: 3000,
