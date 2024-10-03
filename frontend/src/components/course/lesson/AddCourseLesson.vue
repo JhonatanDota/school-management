b9f64a4f32330c71b41058157e9cf502a8bb17e0
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-10-03 19:14:28 -0300

    course lesson frontend: smoth design changes

--- frontend/src/components/course/lesson/AddCourseLesson.vue
+++ frontend/src/components/course/lesson/AddCourseLesson.vue
@@ -1,7 +1,7 @@
 <template>
     <button class="" @click="handleFormVisibility">
         <PlusIcon class="w-10 h-10 md:w-14 md:h-14" fill="white" v-if="!showForm" />
-        <CancelIcon class="w-10 h-10" fill="white" v-else />
+        <XIcon class="w-10 h-10" fill="white" v-else />
     </button>
     <div class="flex flex-col gap-1" v-if="showForm">
         <InputLabel text="Nome" track="course-lesson-name" />
@@ -21,7 +21,7 @@ import { addCourseLesson } from '@/requests/courseLessonRequests';
 import InputLabel from '@/components/common/form/inputs/InputLabel.vue';
 import InputText from '@/components/common/form/inputs/InputText.vue';
 import PlusIcon from '@/icons/PlusIcon.vue';
-import CancelIcon from '@/icons/CancelIcon.vue';
+import XIcon from '@/icons/XIcon.vue';
 import SubmitButton from '@/components/common/form/SubmitButton.vue';
 
 interface addCourseLessonProps {
--- frontend/src/components/course/lesson/CourseLesson.vue
+++ frontend/src/components/course/lesson/CourseLesson.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="border-2 bg-green-600 p-2">
-        <h1>{{ courseLesson.name }}</h1>
+    <div
+        class="flex justify-between items-center rounded-l-md border-l-8 border-l-white bg-green-600 p-3 cursor-pointer">
+        <span class="text-base md:text-lg font-medium">{{ courseLesson.name }}</span>
+        <button class="w-7 h-7 md:w-9 md:h-9">
+            <XIcon class="w-full h-full" fill="white" />
+        </button>
     </div>
 </template>
 
 <script setup lang="ts">
 import { defineProps } from 'vue';
 import { CourseLessonModel } from '@/models/CourseLessonModel';
+import XIcon from '@/icons/XIcon.vue';
 
 interface CourseLessonProps {
     courseLesson: CourseLessonModel
