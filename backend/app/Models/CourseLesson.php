2764a2ebef6aa2d50cacc5f6cc7d4084091f9568
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-10-21 18:54:54 -0300

    backend course lesson: add has batch trait

--- backend/app/Models/CourseLesson.php
+++ backend/app/Models/CourseLesson.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use App\Models\Model;
+
+use Mavinoo\Batch\Traits\HasBatch;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CourseLesson extends Model
 {
     use HasFactory;
+    use HasBatch;
 
     /**
      * The attributes that are mass assignable.
