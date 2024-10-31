d9d1be93133d4e5a1a59fe6ed6c49c8a5ae01faa
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-10-31 20:42:14 -0300

    frontend course lesson: init edit and delete buttons

--- frontend/src/components/course/lesson/CourseLesson.vue
+++ frontend/src/components/course/lesson/CourseLesson.vue
@@ -1,22 +1,38 @@
 <template>
-    <div
-        class="flex justify-between items-center rounded-l-md border-l-8 border-l-white bg-green-600 p-3 cursor-pointer select-none">
-        <span class="text-base md:text-lg font-medium">{{ courseLesson.name }}</span>
-        <button class="w-7 h-7 md:w-9 md:h-9">
-            <XIcon class="w-full h-full" fill="white" />
-        </button>
+    <div class="flex justify-between items-center rounded-md border-2 border-green-600 p-2 cursor-move select-none">
+        <span class="text-base md:text-lg font-bold text-white">{{ courseLesson.name }}</span>
+
+        <div class="flex gap-2">
+            <button @click="handleEdit" class="w-5 h-5 md:w-9 md:h-9">
+                <EditIcon class="w-full h-full fill-yellow-400" />
+            </button>
+
+            <button @click="handleDelete" class="w-5 h-5 md:w-9 md:h-9">
+                <TrashIcon class="w-full h-full fill-red-600" />
+            </button>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { defineProps } from 'vue';
 import { CourseLessonModel } from '@/models/CourseLessonModel';
-import XIcon from '@/icons/XIcon.vue';
+import TrashIcon from '@/icons/TrashIcon.vue';
+import EditIcon from '@/icons/EditIcon.vue';
 
 interface CourseLessonProps {
     courseLesson: CourseLessonModel
 }
 
 const props = defineProps<CourseLessonProps>();
+
+function handleEdit() {
+    console.log(props.courseLesson);
+}
+
+function handleDelete() {
+    console.log(props.courseLesson);
+}
+
 
 </script>
