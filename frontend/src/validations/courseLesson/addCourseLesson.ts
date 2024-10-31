bd0841eade2bf12e56891ec0c5a97165233c242b
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-10-31 20:38:52 -0300

    frontend course lesson: add validation

--- frontend/src/validations/courseLesson/addCourseLesson.ts
+++ frontend/src/validations/courseLesson/addCourseLesson.ts
@@ -2,6 +2,9 @@ import BaseValidations from "../baseValidation";
 import { CourseLessonAddModel } from "@/models/CourseLessonModel";
 
 export default class AddCourseLessonValidation extends BaseValidations {
+  readonly NAME_MIN_LENGTH: number = 3;
+  readonly NAME_MAX_LENGTH: number = 20;
+
   courseLessonData: CourseLessonAddModel;
 
   constructor(courseLessonData: CourseLessonAddModel) {
@@ -13,7 +16,18 @@ export default class AddCourseLessonValidation extends BaseValidations {
   }
 
   checkValidations(): void {
-    if (!this.courseLessonData.name)
+    if (!this.courseLessonData.name) {
       this.addError({ field: "Nome", message: "Preencha este campo" });
+    } else if (this.courseLessonData.name.length < this.NAME_MIN_LENGTH) {
+      this.addError({
+        field: "Nome",
+        message: `Deve ter no mínimo ${this.NAME_MIN_LENGTH} caracteres.`,
+      });
+    } else if (this.courseLessonData.name.length > this.NAME_MAX_LENGTH) {
+      this.addError({
+        field: "Nome",
+        message: `Pode ter no máximo ${this.NAME_MAX_LENGTH} caracteres.`,
+      });
+    }
   }
 }
