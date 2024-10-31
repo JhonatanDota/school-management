fadb8956abc3c3074d9ab3ee465e3225ec641e84
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-10-31 20:38:33 -0300

    frontend config: add vue-final-modal to main file

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,13 +5,16 @@ import router from "./router";
 import Vue3Toasity, { type ToastContainerOptions } from "vue3-toastify";
 import { plugin as Slicksort } from "vue-slicksort";
 import { createPinia } from "pinia";
+import { createVfm } from "vue-final-modal";
 import "./index.css";
+import 'vue-final-modal/style.css'
 
 createApp(App)
   .use(router)
   .use(MotionPlugin)
   .use(Slicksort)
   .use(createPinia())
+  .use(createVfm())
   .use(Vue3Toasity, {
     autoClose: 3000,
   } as ToastContainerOptions)
